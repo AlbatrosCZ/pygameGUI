aa0697ffcf567ef5512f6e6948ff45753b08e04e
AlbatrosCZ <dan-pavlik@seznam.cz>
2021-04-23 09:49:15 +0200

    add Window

--- __main__.py
+++ __main__.py
@@ -1,7 +1,21 @@
 import pygame
 import string
 
-from time import now as t
+from time import time as now
 
 pygame.init()
 pygame.mixer.init()
+
+class Window:
+    def __init__(self, width: int, height: int, fullscreen: bool = False):
+
+        if fullscreen:
+            self.root = pygame.display.set_mode([width, height], pygame.FULLSCREEN)
+        else:
+            self.root = pygame.display.set_mode([width, height])
+
+        self.width, self.height = self.root.get_width(), self.root.get_height()
+        self.clock = pygame.time.Clock()
+        
+        self.images = {}
+        self.fonts = {}
